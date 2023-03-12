90ef024f8e474510decc125217afc7178859556e
Arun6121143 <110023519+Arun6121143@users.noreply.github.com>
2023-03-12 21:18:53 +0530

    Delete deploy directory

--- deploy/src/main/java/com/example/deploy/dao/DeployRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.deploy.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.deploy.model.Deploy;
-
-public interface DeployRepository extends JpaRepository<Deploy,Integer> {
-    
-}
